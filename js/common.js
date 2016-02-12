763addc9f3118e1dce9744636f7a0b58adb73d96
web-raWWWr <webrawwwr@gmail.com>
2016-02-13 01:35:11 +0200

    Готовый хедер, футер, розметка секций.

--- js/common.js
+++ js/common.js
@@ -1,12 +1,43 @@
 $(document).ready(function() {
 	
 	function heightDetect() {
-		$(".main_head").css("height", $(window).height());
+		$(".main_head").css("min-height", $(window).height());
 	};
 	heightDetect();
 	$(window).resize(function() {
 		heightDetect();
 	});
 	
+	$(".toggle_mnu").click(function() {
+		$(".sandwich").toggleClass("active");
+	});
+
+	$(".top_mnu ul a").click(function() {
+		$(".top_mnu").fadeOut(600);
+		$(".sandwich").toggleClass("active");
+		$(".top_text").css("opacity", "1");
+	}).append("<span>");
+
+	$(".toggle_mnu").click(function() {
+		if ($(".top_mnu").is(":visible")) {
+			$(".top_text").css("opacity", "1");
+			$(".top_mnu").fadeOut(600);
+			$(".top_mnu li a").removeClass("fadeInUp animated");
+		} else {
+			$(".top_text").css("opacity", ".1");
+			$(".top_mnu").fadeIn(600);
+			$(".top_mnu li a").addClass("fadeInUp animated");
+		};
+	});
+	
 	
 });
+$(window).load(function() {
+
+	$(".loader_inner").fadeOut();
+	$(".loader").delay(1000).fadeOut("slow");
+
+	// $(".top_text h1").animated("fadeInDown", "fadeOutUp");
+	// $(".top_text p").animated("fadeInUp", "fadeOutDown");
+
+}); 
